16667264721d48c6664d819d01a3d4197b79ea41
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-20 08:12:14 +0700

    Update and rename userbot/modules/.bin/blacklist.py to userbot/modules/blacklist.py

--- userbot/modules/blacklist.py
+++ userbot/modules/blacklist.py
@@ -1,11 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-
-# port to userbot from uniborg by @keselekpermen69
-
-
 import io
 import re
 
