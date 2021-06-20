24398ba8d025f33cf9aae369760a6b0a154c1c1a
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-20 08:06:31 +0700

    Update and rename userbot/modules/.bin/anime.py to userbot/modules/anime.py

--- userbot/modules/anime.py
+++ userbot/modules/anime.py
@@ -1,8 +1,3 @@
-# Copyright (C) 2020 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
 
 import asyncio
 import html
