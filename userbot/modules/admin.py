0cc7b67d07d919a2a83486d3c5f331e13a296c9e
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-20 08:03:11 +0700

    Update and rename userbot/modules/.bin/admin.py to userbot/modules/admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -1,10 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-
-from asyncio import sleep
-from os import remove
 
 from telethon.errors import (
     BadRequestError,
