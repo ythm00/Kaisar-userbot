5f722f916ad8bdc59de74badc705a860545b2173
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-20 08:15:40 +0700

    Update chat.py

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -1,8 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-""" Userbot module containing userid, chatid and log commands"""
 
 from asyncio import sleep
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
