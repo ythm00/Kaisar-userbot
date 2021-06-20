d7700aa72f5a51fad10d0cb8f1130a9da1ba0370
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-20 07:50:47 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"kaisar-userbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"KAISAR-USERBOT 🔅 V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):
