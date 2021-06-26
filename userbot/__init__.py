67849a3a9727c28637928e3c424773a59e27f63b
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-26 17:59:36 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -345,7 +345,7 @@ ZALG_LIST = {}
 
 def paginate_help(page_number, loaded_modules, prefix):
     number_of_rows = 5
-    number_of_cols = 4
+    number_of_cols = 3
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
