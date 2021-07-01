a81296aef36219fd331761d12c8c441e6678fe13
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-01 20:25:12 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -230,7 +230,8 @@ API_URL = os.environ.get("API_URL", "http://antiddos.systems")
 # Inline bot helper
 BOT_TOKEN = os.environ.get("BOT_TOKEN") or None
 BOT_USERNAME = os.environ.get("BOT_USERNAME") or None
-
+# Button helpme
+BUTTON = os.environ.get("BUTTON", "")
 # Init Mongo
 MONGOCLIENT = MongoClient(MONGO_URI, 27017, serverSelectionTimeoutMS=1)
 MONGO = MONGOCLIENT.userbot
@@ -349,7 +350,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} 🏆".format("🏆", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} 🏆".format(f"{BUTTON}", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
