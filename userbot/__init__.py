486fd8bf8d530281e7bf75257b00b2f9b216023a
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-02 09:03:37 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -173,7 +173,7 @@ BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 TERM_ALIAS = os.environ.get("TERM_ALIAS", "Kaisar-userbot")
 
 # Bot version
-BOT_VER = os.environ.get("BOT_VER", "4.0")
+BOT_VER = os.environ.get("BOT_VER", "5.0")
 
 # Default .alive username
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
