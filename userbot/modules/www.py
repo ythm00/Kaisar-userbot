340d94f4cef578d749593e08b8daa7ee7f384ff9
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-10 07:05:06 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -184,12 +184,12 @@ def speed_convert(size):
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     start = datetime.now()
-    await pong.edit("`ＰＯＮＧ.....:۞:`")
-    await pong.edit("`ＰＯＮＧ....:۞:.`")
-    await pong.edit("`ＰＯＮＧ...:۞:..`")
-    await pong.edit("`ＰＯＮＧ..:۞:...`")
-    await pong.edit("`ＰＯＮＧ.:۞:....`")
-    await pong.edit("`ＰＯＮＧ:۞:.....`")
+    await pong.edit("`ＰＯＮＧ.....❮`")
+    await pong.edit("`ＰＯＮＧ....❮.`")
+    await pong.edit("`ＰＯＮＧ...❮..`")
+    await pong.edit("`ＰＯＮＧ..❮...`")
+    await pong.edit("`ＰＯＮＧ.❮....`")
+    await pong.edit("`ＰＯＮＧ❮.....`")
     end = datetime.now()
     duration = (end - start).microseconds / 9000
     await pong.edit(f"┏━━━━━━༻❁༺━━━━━━┓\n ＫＡＩＳＡＲ-ＵＳＥＲＢＯＴ\n┗━━━━━━༻❁༺━━━━━━┛\n"
