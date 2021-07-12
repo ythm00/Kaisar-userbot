bc52efc482ba745a9e6db301658deb555ab3a91c
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-12 12:42:53 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -409,7 +409,7 @@ with bot:
                 result = builder.photo(
                     fiile=kaisarlogo,
                     link_preview=False,
-                    text=f"┏━━━━━━༻❁༺━━━━━━┓\n ＫＡＩＳＡＲ-ＵＳＥＲＢＯＴ\n┗━━━━━━༻❁༺━━━━━━┛\n┣ 🧩 **MODUL TERSEDIA :** `{}`\n┣ 🧰 **DAFTAR MODUL :** \n┗━━━━━━༻❁༺━━━━━━┛".format(len(dugmeler),
+                    text=f"┏━━━━━━༻❁༺━━━━━━┓\n ＫＡＩＳＡＲ-ＵＳＥＲＢＯＴ\n┗━━━━━━༻❁༺━━━━━━┛\n┣ 🧩 **MODUL TERSEDIA :** `{len(modules)}`\n┣ 🧰 **DAFTAR MODUL :** \n┗━━━━━━༻❁༺━━━━━━┛".format(len(dugmeler),
                     ),
                     buttons=buttons,
                 )
